caf1a75573827589da901f2434e22d4de9e3b80c
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-11-08 23:07:50 +1100

    feat: added latex support

--- init.lua
+++ init.lua
@@ -255,5 +255,12 @@ require'nvim-web-devicons'.setup {
 
 require('kommentary.config').use_extended_mappings()
 
+-- VimTex 
+vim.g.tex_flavor='latex'
+vim.g.vimtex_view_method='zathura'
+vim.g.vimtex_quickfix_mode=0
+vim.api.nvim_command('set conceallevel=2')
+vim.g.tex_conceal='abdmg'
 
+--NvimTree
 vim.api.nvim_command([[autocmd VimEnter * NvimTreeOpen]])
